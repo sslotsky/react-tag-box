c200b1c27427cee953d57c0d384fcc4b127d9ec0
Sam Slotsky <saxosamo@gmail.com>
2016-11-10 20:37:59 -0600

    Autocomplete tagging proof of concept

---
+++ app/App.jsx
@@ -0,0 +1,75 @@
+import { Set } from 'immutable'
+import React, { Component } from 'react'
+import Autocomplete from './Autocomplete'
+
+
+export default class App extends Component {
+  state = {
+    tag: '',
+    tags: Set.of('foo', 'bar', 'baz'),
+    selected: Set.of('foo'),
+    open: false
+  }
+
+  render() {
+    const { tag, tags, selected, open } = this.state;
+
+    const pills = selected.map(t => (
+      <li key={t}>{t}</li>
+    ))
+
+    const updateTag = e => {
+      this.setState({ tag: e.target.value })
+    }
+
+    const select = (val) => {
+      if (!val) {
+        return
+      }
+
+      this.setState({
+        tag: '',
+        tags: tags.add(val),
+        selected: selected.add(val)
+      })
+    }
+
+    const blurTag = () => {
+      if (open) {
+        return
+      }
+
+      select(tag)
+    }
+
+    const submitTag = e => {
+      if (e.which === 13) {
+        select(tag)
+      }
+    }
+
+    const autocompleteOpen = () => {
+      this.setState({ open: true })
+    }
+
+    const autocompleteClose = () => {
+      this.setState({ open: false })
+    }
+
+    return (
+      <div>
+        <ul>
+          {pills}
+        </ul>
+        <input value={tag} onChange={updateTag} onBlur={blurTag} onKeyUp={submitTag} />
+        <Autocomplete
+          input={tag}
+          tags={tags.toJS()}
+          open={autocompleteOpen}
+          close={autocompleteClose}
+          select={select}
+        /> 
+      </div>
+    )
+  }
+}
---
+++ app/Autocomplete.jsx
@@ -0,0 +1,40 @@
+import React, { PropTypes, Component } from 'react'
+
+export default class extends Component {
+  static propTypes = {
+    input: PropTypes.string,
+    tags: PropTypes.arrayOf(PropTypes.string).isRequired
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { tags, input, open, close } = nextProps;
+
+    if (!input || input === this.props.input) {
+      return
+    }
+
+    if (tags.some(t => t.includes(input))) {
+      open();
+    } else {
+      close();
+    }
+  }
+
+  render() {
+    const { input, tags, select } = this.props
+
+    if (!input) {
+      return false
+    }
+
+    const matching = tags.filter(t => t.includes(input)).map(t => (
+      <li key={t} onClick={() => select(t)}>{t}</li>
+    ))
+
+    return !!matching.length && (
+      <ul>
+        {matching}
+      </ul>
+    )
+  }
+}
