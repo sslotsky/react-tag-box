9ce5fba7c668bcb945a88c732a4b2dd1e7b1be2e
Samuel Slotsky <sam.slotsky@reachlocal.com>
2016-11-11 12:43:57 -0600

    keyboard navigation

--- app/App.jsx
+++ app/App.jsx
@@ -7,13 +7,14 @@ import '../styles.scss'
 export default class App extends Component {
   state = {
     tag: '',
-    tags: Set.of('foo', 'bar', 'baz'),
+    tags: Set.of('foo', 'bar', 'baz', 'blitz', 'quux', 'barf', 'balderdash'),
     selected: Set.of('foo'),
-    open: false
+    open: false,
+    considering: ''
   }
 
   render() {
-    const { tag, tags, selected, open } = this.state
+    const { tag, tags, selected, open, considering } = this.state
 
     const pills = selected.map(t => (
       <li key={t}>{t}</li>
@@ -30,22 +31,45 @@ export default class App extends Component {
 
       this.setState({
         tag: '',
-        tags: tags.add(val),
+        tags: tags.remove(val),
         selected: selected.add(val)
       })
     }
 
     const blurTag = () => {
       if (open) {
-        return
+        select(this.state.considering)
+      } else {
+        select(tag)
       }
-
-      select(tag)
     }
 
-    const submitTag = e => {
-      if (e.which === 13) {
-        select(tag)
+    const keydown = e => {
+      switch(e.which) {
+        case 13:
+          e.preventDefault()
+          if (open) {
+            select(considering)
+          } else {
+            select(tag)
+          }
+          break
+        case 40:
+          this.autocomplete.considerNext()
+          break
+        case 38:
+          this.autocomplete.considerPrevious()
+          break
+        case 9:
+          if (open) {
+            e.preventDefault()
+            if (e.shiftKey) {
+              this.autocomplete.considerPrevious()
+            } else {
+              this.autocomplete.considerNext()
+            }
+          }
+          break
       }
     }
 
@@ -57,18 +81,25 @@ export default class App extends Component {
       this.setState({ open: false })
     }
 
+    const consider = (option) => {
+      this.setState({ considering: option })
+    }
+
     return (
       <div>
         <ul>
           {pills}
         </ul>
-        <input value={tag} onChange={updateTag} onBlur={blurTag} onKeyUp={submitTag} />
+        <input value={tag} onChange={updateTag} onBlur={blurTag} onKeyDown={keydown} />
         <Autocomplete
+          ref={node => this.autocomplete = node}
           input={tag}
           tags={tags.toJS()}
           open={autocompleteOpen}
           close={autocompleteClose}
           select={select}
+          considering={considering}
+          consider={consider}
         />
       </div>
     )
--- app/Autocomplete.jsx
+++ app/Autocomplete.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react'
+import classNames from 'classnames'
 
 export default class extends Component {
   static propTypes = {
@@ -9,30 +10,59 @@ export default class extends Component {
     select: PropTypes.func.isRequired
   }
 
+  considerNext() {
+    const { tags, consider, considering, input } = this.props
+    const matches = tags.filter(t => t.includes(input))
+    const nextIndex = Math.min(matches.length - 1, matches.indexOf(considering) + 1)
+    consider(matches[nextIndex])
+  }
+
+  considerPrevious() {
+    const { tags, consider, considering, input } = this.props
+    const matches = tags.filter(t => t.includes(input))
+    const nextIndex = Math.max(0, matches.indexOf(considering) - 1)
+    consider(matches[nextIndex])
+  }
+
   componentWillReceiveProps(nextProps) {
-    const { tags, input, open, close } = nextProps
+    const { tags, input, open, close, considering, consider } = nextProps
 
     if (!input || input === this.props.input) {
       return
     }
 
-    if (tags.some(t => t.includes(input))) {
+    const matches = tags.filter(t => t.includes(input))
+    if (matches.length) {
       open()
+      consider(matches[0])
     } else {
       close()
     }
   }
 
   render() {
-    const { input, tags, select } = this.props
+    const { input, tags, select, considering, consider } = this.props
 
     if (!input) {
       return false
     }
 
-    const matching = tags.filter(t => t.includes(input)).map(t => (
-      <li key={t} onClick={() => select(t)}>{t}</li>
-    ))
+    const matching = tags.filter(t => t.includes(input)).map(t => {
+      const className = classNames({
+        considering: considering === t
+      })
+
+      return (
+        <li
+          key={t}
+          className={className}
+          onClick={() => select(t)}
+          onMouseOver={() => consider(t)}
+        >
+          {t}
+        </li>
+      )
+    })
 
     return !!matching.length && (
       <ul className="autocomplete">
