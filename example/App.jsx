8f0b5ca7912a0e297dccb6458d37decdfe7ecefc
Sam Slotsky <saxosamo@gmail.com>
2016-11-19 19:57:17 -0600

    Update examples with code samples

--- example/App.jsx
+++ example/App.jsx
@@ -1,19 +1,51 @@
 import React, { Component } from 'react'
+import Codemirror from 'react-codemirror'
+import 'codemirror/mode/jsx/jsx'
+
 import BackspaceDeletion from './BackspaceDeletion'
 import CustomRendering from './CustomRendering'
 import TagRejection from './TagRejection'
 import './styles.scss'
 
+import backspaceDeletion from 'raw!./codeSamples/backspaceDeletion.txt'
+import customRendering from 'raw!./codeSamples/customRendering.txt'
+import tagRejection from 'raw!./codeSamples/tagRejection.txt'
+
 export default class App extends Component {
+  state = {
+    sample: backspaceDeletion
+  }
+
   render() {
+    const config = {
+      mode: 'javascript',
+      theme: 'erlang-dark',
+      readOnly: true
+    }
+
+    const seeCode = code => () =>
+      this.setState({ sample: code })
+
     return (
-      <div style={{ width: '50%' }}>
-        <h1>Backspace Deletion</h1>
-        <BackspaceDeletion />
-        <h1>Custom Rendering</h1>
-        <CustomRendering />
-        <h1>Tag Rejection <small>(try submitting a tag with the @ character)</small></h1>
-        <TagRejection />
+      <div className="row">
+        <div className="col-1">
+          <h1>Backspace Deletion -
+            <small><a onClick={seeCode(backspaceDeletion)}> See Code</a></small>
+          </h1>
+          <BackspaceDeletion />
+          <h1>Custom Rendering -
+            <small><a onClick={seeCode(customRendering)}> See Code</a></small>
+          </h1>
+          <CustomRendering />
+          <h1>Tag Rejection -
+            <small><a onClick={seeCode(tagRejection)}> See Code</a></small>
+          </h1>
+          <TagRejection />
+        </div>
+        <div className="col-1">
+          <h1>Check the code:</h1>
+          <Codemirror options={config} value={this.state.sample} />
+        </div>
       </div>
     )
   }
