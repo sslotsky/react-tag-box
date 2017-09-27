b2ede491c7ecf56c7694d18e91db7ce0d5bb410c
DanAndreasson <dan@danandreasson.se>
2017-09-27 12:39:08 +0200

    Bump to react 16 and use of proptypes

--- src/TagBoxContainer.jsx
+++ src/TagBoxContainer.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react'
+import React, { Component } from 'react'
+import PropTypes from "prop-types"
 import TagProp from './utils'
 import TAG_REJECTED from './constants'
 import TagManager from './TagManager'
